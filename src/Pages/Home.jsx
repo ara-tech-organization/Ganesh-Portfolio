5dc62507699bc6b54965f5b2abf22039289f15c8
yuvayogi-ara <yuvayogi.ara@gmail.com>
2025-08-23 17:13:32 +0530

    update meta og image, title, desc - 230825

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import Herosection from '../Components/Home/Herosection'
-import Header from '../Components/Header'
-import About from '../Components/Home/About'
+import React from "react";
+import Herosection from "../Components/Home/Herosection";
+import Header from "../Components/Header";
+import About from "../Components/Home/About";
+import { Helmet } from "react-helmet-async";
 
-import FooterSection from '../Components/Footer'
+import FooterSection from "../Components/Footer";
 function Home() {
   return (
     <div>
-        <Header/>
-        <div id="home">
-      <Herosection/>
+      <Helmet>
+        <title>Ganesh Prabhu Arivanantham</title>
+        <meta
+          name="description"
+          content="Ganesh Prabhu Arivanantham - Product Manager, Digital Transformation Strategist & Business Analyst. Empowering education, clinics & franchises through automation, strategy & growth."
+        />
+        <meta
+          name="keywords"
+          content="Ganesh Prabhu, Product Manager, Business Analyst, Digital Transformation Strategist, Leadership Development, CRM Automation, Multi-branch Growth, Business Mentor, Thanjavur, Tamil Nadu, IT Services, Digital Marketing, Education Automation"
+        />
+        <meta name="author" content="Ganesh Prabhu Arivanantham" />
+      </Helmet>
+      <Header />
+      <div id="home">
+        <Herosection />
       </div>
       <div id="services">
-      <About/>
+        <About />
       </div>
-      <div id="education" >
-        
-      <FooterSection />
-      
+      <div id="education">
+        <FooterSection />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
